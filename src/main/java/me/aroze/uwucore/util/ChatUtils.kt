4832db79a79774b5b361020742fc88520f79c74b
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-11-06 17:46:49 +0000

    Added String.coloured()

---
+++ src/main/java/me/aroze/uwucore/util/ChatUtils.kt
@@ -0,0 +1,19 @@
+package me.aroze.uwucore.util
+
+import net.md_5.bungee.api.ChatColor
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+
+val hexPattern = Pattern.compile("&(#[a-fA-F\\d]{6})");
+
+fun String.coloured(): String {
+    var coloured = this
+    var match: Matcher = hexPattern.matcher(coloured)
+    while (match.find()) {
+        val color: String = coloured.substring(match.start(), match.end())
+        coloured = coloured.replace(color, ChatColor.of(color.substring(1)).toString())
+        match = hexPattern.matcher(coloured)
+    }
+    return ChatColor.translateAlternateColorCodes('&', coloured)
+}
