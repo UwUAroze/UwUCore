da911f9cf6d211e22368a2c5933bd2adeb0d81c4
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-11-13 22:36:15 +0000

    Changed console heal message, again

--- src/main/java/me/aroze/uwucore/command/HealCommand.kt
+++ src/main/java/me/aroze/uwucore/command/HealCommand.kt
@@ -21,7 +21,7 @@ object HealCommand : CommandExecutor{
         val soft = if (label.contains("s")) " soft" else ""
 
         if (player !== sender) {
-            val healer = if (sender is Player) sender.name else "a weirdo in console"
+            val healer = if (sender is Player) sender.name else "console"
             sender.sendMessage("&#ffd4e3You've$soft healed &#eb9bb7${player.name}&#ffd4e3, aww.".coloured())
             player.sendMessage("&#ffd4e3You've been$soft healed by &#eb9bb7${healer}&#ffd4e3, how cute ;3".coloured())
         } else player.sendMessage("&#ffd4e3You've majestically$soft healed yourself".coloured())
