cc619b8d85781f9b793ec7d2a3dcac8469f9750a
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-11-19 15:43:54 +0000

    Improved uwuify() + fixed things

--- src/main/java/me/aroze/uwucore/util/UwUUtils.kt
+++ src/main/java/me/aroze/uwucore/util/UwUUtils.kt
@@ -6,44 +6,44 @@ fun uwuify(text: String) : String {
 
     var toReturn = text
 
-    toReturn.replaceCaseInsensitive("\\. ", "~ ");
-    toReturn.replaceCaseInsensitive(", ", "~ ");
-    toReturn.replaceCaseInsensitive("- ", "~ ");
-    toReturn.replaceCaseInsensitive("\\? ", "~ ");
-    toReturn.replaceCaseInsensitive("hurt", "hUWUrt");
-    toReturn.replaceCaseInsensitive("kill", "hwuwrt");
-    toReturn.replaceCaseInsensitive("you", "you<3");
-    toReturn.replaceCaseInsensitive("r", "w");
-    toReturn.replaceCaseInsensitive("l", "w");
-    toReturn.replaceCaseInsensitive("uwu", "UWU");
-    toReturn.replaceCaseInsensitive("owo", "OWO");
-    toReturn.replaceCaseInsensitive(";-;", "(-_-)");
-    toReturn.replaceCaseInsensitive("-_-", "(-_-)");
-    toReturn.replaceCaseInsensitive(":o", "※(^o^)/※");
-    toReturn.replaceCaseInsensitive(":0", "※(^o^)/※");
-    toReturn.replaceCaseInsensitive(":\\)", "(｡◕‿‿◕｡)");
-    toReturn.replaceCaseInsensitive(":>", "(｡◕‿‿◕｡)");
-    toReturn.replaceCaseInsensitive(":\\(", "(︶︹︶)");
-    toReturn.replaceCaseInsensitive(":<", "(︶︹︶)");
-    toReturn.replaceCaseInsensitive(":3", "(・3・)");
-    toReturn.replaceCaseInsensitive(":D", "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧");
-    toReturn.replaceCaseInsensitive("\\._\\.", "(っ´ω`c)");
-    toReturn.replaceCaseInsensitive("fuck", "fwick");
-    toReturn.replaceCaseInsensitive("shit", "*poops*");
-    toReturn.replaceCaseInsensitive("wtf", "whawt the fwick");
-    toReturn.replaceCaseInsensitive("wth", "whawt the hecc");
+    toReturn = toReturn.replaceCaseInsensitive("\\. ", "~ ");
+    toReturn = toReturn.replaceCaseInsensitive(", ", "~ ");
+    toReturn = toReturn.replaceCaseInsensitive("- ", "~ ");
+    toReturn = toReturn.replaceCaseInsensitive("\\? ", "~ ");
+    toReturn = toReturn.replaceCaseInsensitive("hurt", "hUWUrt");
+    toReturn = toReturn.replaceCaseInsensitive("kill", "hwuwrt");
+    toReturn = toReturn.replaceCaseInsensitive("you", "you<3");
+    toReturn = toReturn.replaceCaseInsensitive("r", "w");
+    toReturn = toReturn.replaceCaseInsensitive("l", "w");
+    toReturn = toReturn.replaceCaseInsensitive("uwu", "UWU");
+    toReturn = toReturn.replaceCaseInsensitive("owo", "OWO");
+    toReturn = toReturn.replaceCaseInsensitive(";-;", "(-_-)");
+    toReturn = toReturn.replaceCaseInsensitive("-_-", "(-_-)");
+    toReturn = toReturn.replaceCaseInsensitive(":o", "※(^o^)/※");
+    toReturn = toReturn.replaceCaseInsensitive(":0", "※(^o^)/※");
+    toReturn = toReturn.replaceCaseInsensitive(":\\)", "(｡◕‿‿◕｡)");
+    toReturn = toReturn.replaceCaseInsensitive(":>", "(｡◕‿‿◕｡)");
+    toReturn = toReturn.replaceCaseInsensitive(":\\(", "(︶︹︶)");
+    toReturn = toReturn.replaceCaseInsensitive(":<", "(︶︹︶)");
+    toReturn = toReturn.replaceCaseInsensitive(":3", "(・3・)");
+    toReturn = toReturn.replaceCaseInsensitive(":D", "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧");
+    toReturn = toReturn.replaceCaseInsensitive("\\._\\.", "(っ´ω`c)");
+    toReturn = toReturn.replaceCaseInsensitive("fuck", "fwick");
+    toReturn = toReturn.replaceCaseInsensitive("shit", "*poops*");
+    toReturn = toReturn.replaceCaseInsensitive("wtf", "whawt the fwick");
+    toReturn = toReturn.replaceCaseInsensitive("wth", "whawt the hecc");
 
-    when ((ceil(Math.random() * 25)).toInt()) {
-        1 -> toReturn += "~ uwu *nuzzles you*"
-        2 -> toReturn += "~ owo whats this"
-        3 -> toReturn += "~ *kisses you*"
-        4 -> toReturn += "~ *blushes*"
-        5 -> toReturn += "~ *hehe*"
-        6 -> toReturn += "~ meow"
-        7 -> toReturn += "~ owo"
-        8 -> toReturn += "~ uwu"
-        9 -> toReturn += " ;3"
-        10 -> toReturn += "~"
+    toReturn += when ((ceil(Math.random() * 20)).toInt()) {
+        1 -> "~ uwu *nuzzles you*"
+        2 -> "~ owo whats this"
+        3 -> "~ *kisses you*"
+        4 -> "~ *blushes*"
+        5 -> "~ *hehe*"
+        6 -> "~ meow"
+        7 -> "~ owo"
+        8 -> "~ uwu"
+        9 -> " ;3"
+        else -> "~"
     }
 
     return toReturn
