4652d8450c44bbb914eea54017d7e65f2443fccc
UwUAroze <admin@aroze.me>
2022-11-27 19:44:32 +0000

    Various changes/additions to SkinUtils

--- src/main/java/me/aroze/uwucore/util/SkinUtils.kt
+++ src/main/java/me/aroze/uwucore/util/SkinUtils.kt
@@ -3,7 +3,6 @@ package me.aroze.uwucore.util
 import com.mojang.authlib.GameProfile
 import com.mojang.authlib.properties.Property
 import kong.unirest.Unirest
-import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.OfflinePlayer
 import org.bukkit.inventory.ItemStack
@@ -20,13 +19,13 @@ fun fetchSkull(offlinePlayer: OfflinePlayer, getSkull: (ItemStack) -> Unit) {
     getSkull.invoke(skull)
 }
 
-fun getSkullFromTexture(textureString: String) : ItemStack {
+fun getSkullFromBsae64(base64: String) : ItemStack {
 
     val skull = ItemStack(Material.PLAYER_HEAD)
     val meta = skull.itemMeta as SkullMeta
 
     val profile = GameProfile(UUID.randomUUID(), null);
-    profile.properties.put("textures", Property("textures", textureString));
+    profile.properties.put("textures", Property("textures", base64));
 
     val field : Field = meta.javaClass.getDeclaredField("profile")
     field.isAccessible = true
@@ -38,13 +37,25 @@ fun getSkullFromTexture(textureString: String) : ItemStack {
 
 }
 
-fun validateTextureString(textureString: String, checkTexturesApi: Boolean) : Boolean {
+fun getSkullFromURL(url: String) : ItemStack {
+
+    val b64 = Base64.getEncoder().encodeToString("{\"textures\":{\"SKIN\":{\"url\":\"http://textures.minecraft.net/texture/${stripTextureForApi(url)}\"}}}".toByteArray())
+    return getSkullFromBsae64(b64)
+
+}
+
+fun validateTextureURL(url: String) : Boolean {
+
+    if (!"(?:https?:\\/\\/)?textures\\.minecraft\\.net\\/texture\\/(.+)".toRegex().matches(url)) return false
+    return (validateTextureFromApi(url))
+
+}
+
+fun validateTextureBase64(textureString: String, checkTexturesApi: Boolean) : Boolean {
     val b64 : String
 
     try { b64 = String(Base64.getDecoder().decode(textureString)).replace("\"", "") }
     catch (e: Exception) { return false }
-
-    Bukkit.broadcastMessage(b64)
 
     val validB64 = (b64.startsWith("{textures:{SKIN:{url:http://textures.minecraft.net/texture/")
         && b64.endsWith("}}}"))
@@ -56,14 +67,9 @@ fun validateTextureString(textureString: String, checkTexturesApi: Boolean) : Bo
 
 fun validateTextureFromApi(texture: String) : Boolean {
 
-    val strippedTexture = texture
-        .replace("\"", "")
-        .removePrefix("{textures:{SKIN:{url:http://textures.minecraft.net/texture/")
-        .removeSuffix("}}}")
-
     val responseCode : Int
     try {
-        responseCode = Unirest.get("https://textures.minecraft.net/texture/${strippedTexture}")
+        responseCode = Unirest.get("https://textures.minecraft.net/texture/${stripTextureForApi(texture)}")
             .header("Content-Type", "application/json")
             .asJson()
             .status
@@ -71,4 +77,14 @@ fun validateTextureFromApi(texture: String) : Boolean {
 
     return responseCode == 200
 
+}
+
+fun stripTextureForApi(texture: String) : String {
+    return texture
+        .replace("\"", "")
+        .replace("https", "http")
+        .removePrefix("{textures:{SKIN:{url:")
+        .removePrefix("http://")
+        .removePrefix("textures.minecraft.net/texture/")
+        .removeSuffix("}}}")
 }
