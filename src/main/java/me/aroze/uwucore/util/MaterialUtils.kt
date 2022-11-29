c51bb7adb195f457663fcd173255cd6ef04c02b2
UwUAroze <admin@aroze.me>
2022-11-29 19:59:08 +0000

    Added ItemStack#repair and ItemStack#isDamaged

---
+++ src/main/java/me/aroze/uwucore/util/MaterialUtils.kt
@@ -0,0 +1,18 @@
+package me.aroze.uwucore.util
+
+import org.bukkit.Bukkit
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+
+fun ItemStack.repair() : Boolean {
+    if (!this.isDamaged()) return false
+    val meta = this.itemMeta as Damageable
+    meta.damage = 0
+    this.itemMeta = meta
+    return true
+}
+
+fun ItemStack.isDamaged() : Boolean {
+    if (this.itemMeta !is Damageable) return false
+    return ((this.itemMeta as Damageable).damage != 0)
+}
