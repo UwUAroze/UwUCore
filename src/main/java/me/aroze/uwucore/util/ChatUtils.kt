6857ad6fdbbef5f75fc8570af82bc933bd6ce629
UwUAroze <admin@aroze.me>
2023-01-02 00:05:14 +0000

    Added gradient method

--- src/main/java/me/aroze/uwucore/util/ChatUtils.kt
+++ src/main/java/me/aroze/uwucore/util/ChatUtils.kt
@@ -1,5 +1,6 @@
 package me.aroze.uwucore.util
 
+import me.santio.utils.color.Color
 import net.md_5.bungee.api.ChatColor
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -32,4 +33,38 @@ fun String.handlePluralChar(amount: Int, plural: String = "s"): String {
 
 fun String.handlePluralWord(amount: Int, plural: String): String {
     return if (amount == 1) this else plural
+}
+
+fun Color.toMinecraft() : String = "&${this.toHex()}"
+
+fun String.gradient(start: Color, end: Color): String {
+
+    val length = this.length
+
+    val startRed = start.red
+    val startGreen = start.green
+    val startBlue = start.blue
+
+    val endRed = end.red
+    val endGreen = end.green
+    val endBlue = end.blue
+
+    val stepRed = (endRed - startRed) / length
+    val stepGreen = (endGreen - startGreen) / length
+    val stepBlue = (endBlue - startBlue) / length
+
+    var red = startRed
+    var green = startGreen
+    var blue = startBlue
+
+    val builder = StringBuilder()
+    while (builder.length < length) {
+        builder.append(Color.fromRGB(red, green, blue).toMinecraft() + this[builder.length])
+        red += stepRed
+        green += stepGreen
+        blue += stepBlue
+    }
+
+    return builder.toString()
+
 }
