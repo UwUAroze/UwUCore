b679f465ad37f4a25803994997f8fea98e83acc5
UwUAroze <admin@aroze.me>
2023-01-07 21:24:08 +0000

    Added custom colour codes (&p and &s)

--- src/main/java/me/aroze/uwucore/util/ChatUtils.kt
+++ src/main/java/me/aroze/uwucore/util/ChatUtils.kt
@@ -8,9 +8,17 @@ import java.util.regex.Pattern
 
 val hexPattern = Pattern.compile("&(#[a-fA-F\\d]{6})");
 
+enum class ChatColors(val hex: String) {
+    PRIMARY("&#ffd4e3"),
+    SECONDARY("&#ffb5cf"),
+}
+
 fun String.coloured(): String {
     var coloured = this
-    var match: Matcher = hexPattern.matcher(coloured)
+    var match: Matcher = hexPattern.matcher(coloured
+        .replace("&p", ChatColors.PRIMARY.hex)
+        .replace("&s", ChatColors.SECONDARY.hex))
+
     while (match.find()) {
         val color: String = coloured.substring(match.start(), match.end())
         coloured = coloured.replace(color, ChatColor.of(color.substring(1)).toString())
